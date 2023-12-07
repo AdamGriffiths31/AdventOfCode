0e4e8a456edc8f6d8b9f797f8f525531d75106e3
Adam Griffiths[user] <adam.j.griffiths@outlook.com>
2023-12-07 21:42:13 +0000

    Day 7 2023

---
+++ 2023/Day7/part2.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var letters = map[string]string{
+	"T": "A",
+	"J": ".",
+	"Q": "C",
+	"K": "D",
+	"A": "E",
+}
+
+type Hand struct {
+	cards          string
+	originalCards  string
+	bid            int
+	classification int
+}
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(input), "\n")
+	hands := []Hand{}
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		parts := strings.Split(line, " ")
+		cards := parts[0]
+		bid := parts[1]
+		bidInt, _ := strconv.Atoi(bid)
+		hands = append(hands, Hand{cards, cards, bidInt, 0})
+	}
+	strength(hands)
+}
+
+func strength(hand []Hand) {
+	for i, h := range hand {
+		var replacedCards strings.Builder
+		for _, c := range h.cards {
+			if replacement, ok := letters[string(c)]; ok {
+				replacedCards.WriteString(replacement)
+			} else {
+				replacedCards.WriteRune(c)
+			}
+		}
+		hand[i].cards = replacedCards.String()
+	}
+	for i, h := range hand {
+		hand[i].classification = classify(h)
+	}
+
+	sort.SliceStable(hand, func(i, j int) bool {
+		if hand[i].classification != hand[j].classification {
+			return hand[i].classification < hand[j].classification
+		}
+		return hand[i].cards < hand[j].cards
+	})
+
+	total := 0
+	for i, h := range hand {
+		fmt.Printf("%s %d %d\n", h.originalCards, h.bid, h.classification)
+		total += (i + 1) * h.bid
+	}
+	fmt.Println(total)
+}
+
+func score(hand string) int {
+	counts := count(hand)
+
+	if contains(counts, 5) {
+		return 6
+	}
+	if contains(counts, 4) {
+		return 5
+	}
+	if contains(counts, 3) && contains(counts, 2) {
+		return 4
+	}
+	if contains(counts, 3) {
+		return 3
+	}
+	if countOccurrences(counts, 2) == 4 {
+		return 2
+	}
+	if contains(counts, 2) {
+		return 1
+	}
+
+	return 0
+}
+
+func classify(hand Hand) int {
+	replacementHands := replacements(hand.originalCards)
+	maxScore := 0
+
+	for _, replacement := range replacementHands {
+		currentScore := score(replacement)
+		if currentScore > maxScore {
+			maxScore = currentScore
+		}
+	}
+
+	return maxScore
+
+}
+func replacements(hand string) []string {
+	if hand == "" {
+		return []string{""}
+	}
+
+	var result []string
+	firstCard := string(hand[0])
+
+	if firstCard == "J" {
+		firstCard = "23456789TQKA"
+	}
+
+	for _, x := range firstCard {
+		for _, y := range replacements(hand[1:]) {
+			result = append(result, string(x)+y)
+		}
+	}
+
+	return result
+}
+
+func count(card string) []int {
+	counts := map[string]int{}
+	for _, c := range card {
+		counts[string(c)]++
+	}
+
+	returnVal := []int{}
+	for _, c := range card {
+		returnVal = append(returnVal, counts[string(c)])
+	}
+	return returnVal
+}
+
+func contains(slice []int, value int) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func countOccurrences(slice []int, value int) int {
+	count := 0
+	for _, v := range slice {
+		if v == value {
+			count++
+		}
+	}
+	return count
+}
