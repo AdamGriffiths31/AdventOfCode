6f4322ec1182b6853bddad5bcef8d8f3e69ee471
Adam Griffiths[user] <adam.j.griffiths@outlook.com>
2024-07-16 18:07:35 +0100

    Day 1 Part 2

---
+++ 2023/Day1/day1Part2.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+
+const alloc = std.heap.page_allocator;
+
+const nums = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+pub fn main() !void {
+    const max_bytes_per_line = 4096;
+    var file = std.fs.cwd().openFile("input.txt", .{}) catch {
+        return;
+    };
+    defer file.close();
+    var buffered_reader = std.io.bufferedReader(file.reader());
+    const reader = buffered_reader.reader();
+    var returnValue: i32 = 0;
+    while (try reader.readUntilDelimiterOrEofAlloc(alloc, '\n', max_bytes_per_line)) |line| {
+        defer alloc.free(line);
+        returnValue += findFirstAndLast(line);
+    }
+    std.debug.print("{d}", .{returnValue});
+}
+
+fn findFirstAndLast(line: []u8) i32 {
+    var firstValue: ?u8 = null;
+    var lastValue: ?u8 = null;
+    for (line, 0..) |lineValue, i| {
+        var digit: ?u8 = null;
+        if (isDigit(lineValue)) {
+            digit = lineValue - '0';
+            if (firstValue == null) {
+                firstValue = digit;
+            }
+            lastValue = digit;
+        } else {
+            for (nums, 1..) |str, d| {
+                if (std.mem.startsWith(u8, line[i..], str)) {
+                    digit = @intCast(d);
+                }
+                if (digit != null) {
+                    if (firstValue == null) {
+                        firstValue = digit;
+                    }
+                    lastValue = digit;
+                }
+            }
+        }
+    }
+
+    return 10 * firstValue.? + lastValue.?;
+}
+
+fn isDigit(char: u8) bool {
+    return char >= '0' and char <= '9';
+}
